9f70641c3179187543fa6beff20eb8e199c51f0b
Dimitar Naydenov <mitkoik@gmail.com>
2022-11-26 20:28:47 +0200

    add interceptor

--- webui-angluar/src/app/helpers/http.interceptor.ts
+++ webui-angluar/src/app/helpers/http.interceptor.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { UserService } from '../services/user.service';
 
@@ -8,12 +8,20 @@ export class HttpRequestInterceptor implements HttpInterceptor {
   constructor(private userService: UserService) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    req = req.clone({
-      setHeaders: {
-        Authorization: "Bearer " + this.userService.getToken()
+    
+    if(this.userService.isLogged())
+    {
+      req = req.clone({
+        setHeaders: {
+          Authorization: "Bearer " + this.userService.getToken()
+      }
+      });
     }
-    });
-
+      
     return next.handle(req);
   }
 }
+
+export const httpInterceptorProviders = [
+  { provide: HTTP_INTERCEPTORS, useClass: HttpRequestInterceptor, multi: true },
+];
