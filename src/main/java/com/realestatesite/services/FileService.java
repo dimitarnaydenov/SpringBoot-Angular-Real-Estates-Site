88599b31b46f080e8f99bdb7ba7403ceccbd1614
Dimitar Naydenov <mitkoik@gmail.com>
2022-11-28 13:14:12 +0200

    Create FileService

---
+++ src/main/java/com/realestatesite/services/FileService.java
@@ -0,0 +1,69 @@
+package com.realestatesite.services;
+
+import com.realestatesite.model.Photo;
+import com.realestatesite.repositories.FileRepository;
+import com.realestatesite.repositories.PhotoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+@Service
+public class FileService implements FileRepository {
+    @Autowired
+    PhotoRepository photoRepository;
+
+    private final String root = "src/main/resources/static/property-pictures/";
+
+    @Override
+    public Photo save(MultipartFile file, int propertyId) throws IOException {
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        Path uploadDir = Paths.get(root + propertyId +"/");
+
+        if (!Files.exists(uploadDir)) {
+            Files.createDirectories(uploadDir);
+        }
+
+        try (InputStream inputStream = file.getInputStream()) {
+            Path filePath = uploadDir.resolve(fileName);
+            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+            return photoRepository.save(new Photo( fileName));
+        } catch (IOException ioe) {
+            throw new IOException("Could not save image file: " + fileName, ioe);
+        }
+    }
+
+    @Override
+    public void deleteByPropertyId(int propertyId) {
+        Path deleteDir = Paths.get(root + propertyId +"/");
+        FileSystemUtils.deleteRecursively(deleteDir.toFile());
+    }
+
+    @Override
+    public Resource load(String filename, int propertyId) {
+        Path loadDir = Paths.get(root + propertyId +"/");
+        try {
+            Path file = loadDir.resolve(filename);
+            Resource resource = new UrlResource(file.toUri());
+
+            if (resource.exists() || resource.isReadable()) {
+                return resource;
+            } else {
+                throw new RuntimeException("Could not read the file!");
+            }
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Error: " + e.getMessage());
+        }
+    }
+}
