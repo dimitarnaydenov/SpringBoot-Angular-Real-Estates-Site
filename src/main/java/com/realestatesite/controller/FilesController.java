81491507382d224bc22ca35ad01fb4454908c264
Dimitar Naydenov <mitkoik@gmail.com>
2022-11-28 13:15:07 +0200

    Create FilesController

---
+++ src/main/java/com/realestatesite/controller/FilesController.java
@@ -0,0 +1,25 @@
+package com.realestatesite.controller;
+
+
+import com.realestatesite.repositories.FileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class FilesController {
+
+    @Autowired
+    FileRepository fileRepository;
+
+    @GetMapping("/images/{propertyId:.+}/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> getFile(@PathVariable String propertyId, @PathVariable String filename) {
+        Resource file = fileRepository.load(filename, Integer.parseInt(propertyId));
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
+}
