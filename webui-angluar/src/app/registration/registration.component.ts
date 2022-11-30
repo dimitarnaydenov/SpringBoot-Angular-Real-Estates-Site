f43c1d7c8be099f539c2d0b40b33e531292056f4
Dimitar Naydenov <mitkoik@gmail.com>
2022-11-30 16:48:30 +0200

    add register validation

--- webui-angluar/src/app/registration/registration.component.ts
+++ webui-angluar/src/app/registration/registration.component.ts
@@ -9,19 +9,29 @@ import { UserService } from '../services/user.service';
 })
 export class RegistrationComponent implements OnInit {
 
+  public notSamePasswords = false;
+
+  public error:boolean = false;
+
+  public errorMessage = "";
+
   constructor(private userService:UserService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   onClickSubmit(data:any) {
-    
-    this.userService.register({"username":data.username,"password":data.password}).subscribe({
-      complete: () => {this.router.navigate(['/login'])},
-      error: err => {
-        alert(err.error)
-      }
-    });
+
+    if(data.password === data.repeatPassword)
+      this.userService.register({"username":data.username,"email":data.email,"password":data.password}).subscribe({
+        complete: () => {this.router.navigate(['/login'])},
+        error: err => {
+          this.error = true;
+          this.errorMessage = err.error
+        }
+      });
+    else
+      this.notSamePasswords = true;
     
  }
 
