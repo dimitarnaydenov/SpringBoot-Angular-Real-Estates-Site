472b348aa5a9a829e2a16766928dae250b381297
Dimitar Naydenov <mitkoik@gmail.com>
2022-11-30 16:19:47 +0200

    update app-routing

--- webui-angluar/src/app/app-routing.module.ts
+++ webui-angluar/src/app/app-routing.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddPropertyComponent } from './add-property/add-property.component';
+import { EditPropertyComponent } from './edit-property/edit-property.component';
+import { AuthGuard } from './helpers/auth/auth.guard';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { MyPropertiesComponent } from './my-properties/my-properties.component';
 import { PropertiesComponent } from './properties/properties.component';
+import { PropertyItemComponent } from './property-item/property-item.component';
 import { RegistrationComponent } from './registration/registration.component';
 
 const routes: Routes = [
-  {path:'',component: PropertiesComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path:'home',component: HomeComponent},
   {path:'login',component: LoginComponent},
-  {path:'register',component: RegistrationComponent}
+  {path:'register',component: RegistrationComponent},
+  {path:'properties',component: PropertiesComponent},
+  {path:'property/:id',component: PropertyItemComponent},
+  {path:'addProperty', component: AddPropertyComponent, canActivate:[AuthGuard]},
+  {path:'editProperty/:id',component: EditPropertyComponent, canActivate:[AuthGuard]},
+  {path:'myProperties',component: MyPropertiesComponent, canActivate:[AuthGuard]}
 
 ];
 
