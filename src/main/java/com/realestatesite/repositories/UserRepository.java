4eaf5a61f3979a7e5dd4b8031758bb77954c5089
Dimitar Naydenov <mitkoik@gmail.com>
2023-02-06 10:49:32 +0200

    Update UserRepository.java

--- src/main/java/com/realestatesite/repositories/UserRepository.java
+++ src/main/java/com/realestatesite/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package com.realestatesite.repositories;
 import com.realestatesite.model.CustomUser;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<CustomUser, Integer> {
@@ -10,4 +11,6 @@ public interface UserRepository extends CrudRepository<CustomUser, Integer> {
     Optional<CustomUser> findByUsername(String username);
 
     Optional<CustomUser> findByEmail(String email);
+
+    List<CustomUser> findAll();
 }
